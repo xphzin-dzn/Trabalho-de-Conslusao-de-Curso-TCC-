abcd2f0a6d5e0bcf12dd144677a5c72048872842
graxyzr <greiceeszz@gmail.com>
2025-09-04 15:20:29 -0300

    att

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware'); // Certifique-se que importa o middleware
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+
+// --- NOVA ROTA PROTEGIDA ABAIXO ---
+router.put('/change-password', authMiddleware, authController.changePassword);
 
 module.exports = router;
